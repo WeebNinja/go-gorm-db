dcc28b4d2647c253edae4ff76230c0c4d68a4912
WeebNinja <Nattaponjaijina2002@gmail.com>
2024-01-25 23:22:48 +0700

    golang update

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/WeebNinja/go-gorm-db/db"
 	"github.com/WeebNinja/go-gorm-db/models"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -47,6 +49,15 @@ func main() {
 	// Initialize Gin router
 	r := gin.Default()
 
+	// กำหนด cors (Cross-Origin Resource Sharing)
+	r.Use(cors.New(cors.Config{
+		// 3000 คือ port ที่ใช้งานใน frontend react
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	// Item routes
 	r.GET("/items", itemRepo.GetItems)
 	r.POST("/items", itemRepo.PostItem)
@@ -74,7 +85,7 @@ func main() {
 	})
 
 	// Run the server
-	if err := r.Run(":8000"); err != nil {
+	if err := r.Run(":5000"); err != nil {
 		log.Fatalf("Server is not running: %v", err)
 	}
 }
